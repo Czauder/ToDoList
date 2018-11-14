5d6de1a032ac0253c800ada505148f71d7e3a830
Przemek Czauderna <przemek.czauderna@gmail.com>
2018-11-14 11:15:27 +0100

    removed comments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const btnForm = document.querySelector('.todo-form');
 const input = document.querySelector('.todo-form-message');
 const toDoList = document.querySelector('.todo-list');
 
-// check value 
+
 const addTask = (e) => {
     e.preventDefault();
     const titleTask = input.value;
@@ -10,42 +10,41 @@ const addTask = (e) => {
 
     console.log(titleTask)
 
-    // add whole puzzle 
-    //  todo-items
+
     const toDo = document.createElement('div');
     toDo.classList.add('todo-element')
 
-    // bar up
+
     const toDoBar = document.createElement('div');
     toDoBar.classList.add('todo-element-bar');
 
-    // data bar
+
     const toDoDate = document.createElement('div');
     toDoDate.classList.add('todo-element-bar');
     const date = new Date();
     const dateText = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + ' godz.: ' + date.getHours() + ':' + date.getMinutes();
     toDoDate.innerText = dateText;
 
-    // add remove button
+
     const toDoDelete = document.createElement('button');
     toDoDelete.classList.add('todo-element-delete');
     toDoDelete.classList.add('button');
     toDoDelete.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-    // move item to bar 
+
     toDoBar.appendChild(toDoDate);
     toDoBar.appendChild(toDoDelete)
 
-    // item + txt 
+
     const toDoText = document.createElement('div');
     toDoText.classList.add('todo-element-text');
     toDoText.innerText = titleTask;
 
-    //connetced all
+
     toDo.appendChild(toDoBar);
     toDo.appendChild(toDoText);
 
-    //move to the list
+
     toDoList.append(toDo);
 
     input.value = "";
@@ -55,7 +54,7 @@ const addTask = (e) => {
 btnForm.addEventListener('submit', addTask)
 
 
-// delete task 
+// remove task 
 toDoList.addEventListener('click', function (e) {
     if (e.target.closest('.todo-element-delete') !== null) {
         const todoElem = e.target.closest('.todo-element');
